d1f58e74b16538a3d05028eed223780c27d3e91b
KARTHIKCHILAKA <101442226+KARTHIKCHILAKA@users.noreply.github.com>
2023-05-13 09:08:08 +0530

    Add files via upload

---
+++ BackEnd/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./routes/ToDoRoute');
+
+const app = express();
+const port = 5500;
+
+app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://Karthik:Karthik@cluster-1.zqkyalg.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log('Database connected !!!');
+}).catch(err => console.log(err.message));
+
+
+app.use(route);
+
+app.listen(port, () => console.log(`running on port : ${port}`));
